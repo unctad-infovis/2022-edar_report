7d5637b6ccb9dee9fd3648d8683b4568a8f20e81
Teemo Tebest <teemo.tebest@gmail.com>
2022-07-05 13:35:28 +0200

    Added dummy content to policy recommendations

--- src/jsx/PolicyRecommendations.jsx
+++ src/jsx/PolicyRecommendations.jsx
@@ -3,17 +3,37 @@ import React from 'react';
 function PolicyRecommendations() {
   return (
     <div>
-      <h3>Policy recommendations</h3>
-      <div className="recommendations_container">
-        <div className="recommendation_container">
-          <p>Bacon ipsum dolor amet corned beef cupim pork strip steak, sausage ball tip shank pork loin chuck kevin salami ribeye buffalo. Landjaeger brisket pig strip steak tri-tip jerky corned beef ham short loin tail pork chuck ball tip cow tenderloin.</p>
+      <div className="content_container">
+        <h3>Policy recommendations</h3>
+        <div className="recommendations_container">
+          <div className="recommendation_container">
+            <p>Improve the capacity of people and firms to emulate and innovate</p>
+          </div>
+          <div className="recommendation_container">
+            <p>Support firms to innovatep</p>
+          </div>
+          <div className="recommendation_container">
+            <p>Expand the set of technologies and infrastructure</p>
+          </div>
+          <div className="recommendation_container">
+            <p>Reinforce linkages between industries and encourage the use of local content and suppliers by domestic firms </p>
+          </div>
+          <div className="recommendation_container">
+            <p>Enhance the access of small and medium-sized enterprises to alternative finance</p>
+          </div>
+          <div className="recommendation_container">
+            <p>Maximize the potential benefits of the African Continental Free Trade Area for export diversification by streamlining with inclusive growth and financial inclusion goals and practices</p>
+          </div>
+          <div className="recommendation_container">
+            <p>Strengthen efforts to improve trade and financial data</p>
+          </div>
+          <div className="recommendation_container">
+            <p>Putting recommendations into practice</p>
+          </div>
         </div>
-        <div className="recommendation_container">
-          <p>Bacon ipsum dolor amet corned beef cupim pork strip steak, sausage ball tip shank pork loin chuck kevin salami ribeye buffalo. Landjaeger brisket pig strip steak tri-tip jerky corned beef ham short loin tail pork chuck ball tip cow tenderloin.</p>
+        <div className="download_section">
+          <button type="button">Download full chapter 2</button>
         </div>
-      </div>
-      <div className="download_section">
-        <button type="button">Download full chapter 2</button>
       </div>
     </div>
   );
